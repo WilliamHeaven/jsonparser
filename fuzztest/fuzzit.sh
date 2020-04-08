3566f774bb233d19290503d1a2f33390d2ebc32c
WilliamHeaven <2403306615@qq.com>
2020-04-08 15:48:10 +0800

    Update fuzzit.sh

--- fuzztest/fuzzit.sh
+++ fuzztest/fuzzit.sh
@@ -26,5 +26,4 @@ wget -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/latest/download/fuzz
 chmod a+x fuzzit
 
 ## upload fuzz target for long fuzz testing on fuzzit.dev server or run locally for regression
-pwd
-./fuzzit create job --type ${1} fuzzitdev/fuzztest/parse_json_fuzz parse_json_fuzz
+./fuzzit create job --type ${1} fuzzitdev/parse_json_fuzz parse_json_fuzz
